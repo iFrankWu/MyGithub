0a53e30f03f2f6047b37ab8488aaa6cc24f24e28
wushexin <wushexin2006@126.com>
2015-12-25 23:32:34 +0800

    add new added property exportable

--- ReportProjectV1.2/src/com/tibco/handle/ExportReportHandler.java
+++ ReportProjectV1.2/src/com/tibco/handle/ExportReportHandler.java
@@ -25,7 +25,7 @@ import com.tibco.util.XLSExport;
  */
 public class ExportReportHandler implements ResultSetHandler {
 	private Hospital hopital ;
-	private String tableHeader[] = "报告单ID,检查日期,姓名,年龄,病历号,主机序列号,手控器序列号,主诉,临床表现,初善仪检查结果,初善仪点探数量,是否绝经,主诉/白带多,主诉/性交出血,主诉/不规则流血,主诉/其他,临床表现/光滑,临床表现/急性炎症,临床表现/肥大,临床表现/息肉,临床表现/糜烂,临床表现/撕裂,临床表现/纳氏腺囊肿,临床表现/白斑,临床表现/可疑癌,临床表现/其他,操作人员,申请医生".split(",");
+	private String tableHeader[] = "报告单ID,检查日期,姓名,年龄,病历号,主机序列号,手控器序列号,主诉,临床表现,初善仪检查结果,初善仪点探数量,是否绝经,主诉/白带多,主诉/性交出血,主诉/不规则流血,主诉/其他,临床表现/光滑,临床表现/急性炎症,临床表现/肥大,临床表现/息肉,临床表现/柱状上皮异位,临床表现/撕裂,临床表现/纳氏腺囊肿,临床表现/白斑,临床表现/可疑癌,临床表现/其他,操作人员,申请医生,主诉/LCT,主诉/HPV,临床表现/接触性出血".split(",");
 	public ExportReportHandler(XLSExport xlsExport) throws DBException{
 		this.xlsExport = xlsExport;
 		HospitalDAO hospitalDAO = new HospitalDAO();
@@ -79,6 +79,10 @@ public class ExportReportHandler implements ResultSetHandler {
 
             xlsExport.setCell(26,rs.getString("doctorName"));
             xlsExport.setCell(27,rs.getString("prescribingDoctorName"));
+            
+            xlsExport.setCell(28,rs.getString("lct"));
+            xlsExport.setCell(29,rs.getString("hpv"));
+            xlsExport.setCell(30,rs.getString("touchbleeding"));
 			
 			rowIndex++;
 		}catch(Exception e){
